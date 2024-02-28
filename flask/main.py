4e6c6e084785545b0d0457ddd918c3e941b36eb6
Jacob Tizel <jacob.tizel@gmail.com>
2024-02-28 13:25:16 -0800

    feat: sample template
    demonstrating how the template software works

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, world!"
+    return "Hello World!"
+
+@app.route("/index")
+def index():
+    return render_template("index.html", sample_text="Yo Country")
