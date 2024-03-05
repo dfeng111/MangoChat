7ef2211df37019b235dbedebd2612be8f6c7ff5c
Christopher Hill <chrishill_12@hotmail.com>
2024-03-05 14:03:31 -0800

    Removed about.html from routes.py

--- flask/routes.py
+++ flask/routes.py
@@ -24,9 +24,5 @@ posts = [
 def home():
     return render_template('home.html', posts=posts)
 
-@app.route('/about')
-def about():
-    return render_template('about.html', title='About')
-
 if __name__ == '__main__':
     app.run(debug=True)
