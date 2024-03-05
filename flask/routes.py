6d95f2fb30b8071c011d89209a8d763fbe4ecf37
Christopher Hill <chrishill_12@hotmail.com>
2024-03-05 13:58:49 -0800

    Simplified routes.py

--- flask/routes.py
+++ flask/routes.py
@@ -1,28 +1,32 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from flask import app
-from flask.forms import LoginForm
+from flask import render_template
+from flask import Flask
+
+app = Flask(__name__)
+
+# Sample data for demonstration
+posts = [
+    {
+        'author': 'Chris',
+        'title': 'Blog Post 1',
+        'content': 'Coding is fun.',
+        'date_posted': 'March 04, 2024'
+    },
+    {
+        'author': 'Chris',
+        'title': 'Blog Post 2',
+        'content': 'Coding is not fun.',
+        'date_posted': 'March 05, 2024'
+    }
+]
 
 @app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
