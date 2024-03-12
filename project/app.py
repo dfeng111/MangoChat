2ee1efe8168290964ae3ba8f1d602459ca5d8b24
Jacob Tizel <jacob.tizel@gmail.com>
2024-03-12 14:55:53 -0700

    fixup(sql): Database capitalized

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from database.database_setup import db, User, Channel, UserChannel, Message, Friend, Block
+from Database.database_setup import db, User, Channel, UserChannel, Message, Friend, Block
 
 app = Flask(__name__, static_url_path='/static')
 
