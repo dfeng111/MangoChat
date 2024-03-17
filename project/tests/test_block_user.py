eec03fece79dc18abedf511434b67f095c49ba1e
chrishill93 <113214219+chrishill93@users.noreply.github.com>
2024-03-17 13:28:07 -0700

    Created app fixture and imported pytest to properly set up database in test_block_user.py

--- project/tests/test_block_user.py
+++ project/tests/test_block_user.py
@@ -1,6 +1,23 @@
+import pytest
 from flask import Flask
 from Database.database_setup import db, User, Friend, Block
 from project.block_user import block_user, unblock_user
+
+# Creating the Flask app and setting up the database
+@pytest.fixture
+def app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    yield app
+
+    with app.app_context():
+        db.drop_all()
 
 def test_block_user(app):
     with app.app_context():
