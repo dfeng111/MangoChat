986fec96e09649fb4ee4b64e5f78ebdf110abf60
jacobtizel <123128592+jacobtizel@users.noreply.github.com>
2024-03-18 12:31:00 -0700

    Revert "Add/remove friend functionality"

--- project/Database/database_setup.py
+++ project/Database/database_setup.py
@@ -32,17 +32,17 @@ class Message(db.Model):
 
 class Friend(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id1 = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id2 = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     status = db.Column(db.Enum('Pending', 'Accepted', 'Blocked'), nullable=False, default='Pending')
-    user1 = db.relationship('User', foreign_keys=[user_id])
-    user2 = db.relationship('User', foreign_keys=[friend_id])
-    db.UniqueConstraint('user_id', 'friend_id', name='unique_friendship')
+    user1 = db.relationship('User', foreign_keys=[user_id1])
+    user2 = db.relationship('User', foreign_keys=[user_id2])
+    db.UniqueConstraint('user_id1', 'user_id2', name='unique_friendship')
 
 class Block(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    blocked_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    blocker = db.relationship('User', foreign_keys=[user_id])
-    blocked = db.relationship('User', foreign_keys=[blocked_user_id])
+    blocker_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    blocked_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    blocker = db.relationship('User', foreign_keys=[blocker_id])
+    blocked = db.relationship('User', foreign_keys=[blocked_id])
     db.UniqueConstraint('blocker_id', 'blocked_id', name='unique_block')
