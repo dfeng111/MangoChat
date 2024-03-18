99c534c59373dad39ced96bb0ac98e095a698fc7
chrishill93 <113214219+chrishill93@users.noreply.github.com>
2024-03-18 13:54:10 -0700

    Added Create channel and Delete channel routes in app.py.  Added utils.py file for methods to get current user id, and to check if current user is admin (to support creation and deletion of channels)

---
+++ project/utils.py
@@ -0,0 +1,30 @@
+from flask import session
+
+def get_current_user_id():
+    """
+    Get the ID of the currently logged-in user from the session.
+
+    Returns:
+        User ID if the user is logged in, otherwise None.
+    """
+    return session.get('user_id')
+
+def is_user_channel_admin(user_id, channel_id):
+    """
+    Check if the user with the given user_id is an admin of the channel with the given channel_id.
+
+    Args:
+        user_id: ID of the user
+        channel_id: ID of the channel
+
+    Returns:
+        True if the user is an admin of the channel, False otherwise.
+    """
+    # Query the UserChannel table to check if the user is an admin of the channel
+    user_channel = UserChannel.query.filter_by(user_id=user_id, channel_id=channel_id).first()
+
+    # Check if the user_channel exists and the user is a moderator
+    if user_channel and user_channel.is_moderator:
+        return True
+    else:
+        return False
