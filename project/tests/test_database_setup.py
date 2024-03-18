c77a17885c92140b86645b766a4c855139414ddc
Jacob Tizel <jacob.tizel@gmail.com>
2024-03-18 02:22:56 -0700

    fix(test): all working
    Notes: Only use with app.app_context once, and make sure to commit after every DB interaction

--- project/tests/test_database_setup.py
+++ project/tests/test_database_setup.py
@@ -1,33 +1,25 @@
 import pytest
-# from flask import Flask
 from project.app import app
 from Database.database_setup import db, User, Channel, UserChannel, Message, Friend, Block
 
-# Creating the Flask app and setting up the database
-@pytest.fixture
-def test_app():
-    # app = Flask(__name__)
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mango:COSC310=mcpw@127.0.0.1/mangochat'
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # db.init_app(app)
-    #
-    # with app.app_context():
-    #     db.create_all()
+# Register the function to be run after each test
+@pytest.fixture(autouse=True)
+def setup():
     with app.app_context():
-        yield app
-
-    # with app.app_context():
-        # db.session.remove()
-        # db.drop_all()
+        yield
+    # pass
 
 # Creating a test user
 @pytest.fixture
 def create_test_user():
-    test_user = User(username="test_user", password="test_password")
+    uname = "test_user"
+    test_user = User(username=uname, password="test_password")
     db.session.add(test_user)
     db.session.commit()
     yield test_user
+    # db.session.query(User).filter_by(username="test_user").delete()
     db.session.delete(test_user)
+    db.session.commit()
 
 # Creating a test channel
 @pytest.fixture
@@ -37,16 +29,17 @@ def create_test_channel():
     db.session.commit()
     yield test_channel
     db.session.delete(test_channel)
+    db.session.commit()
 
 # Test cases for database models
-def test_user_creation(test_app, create_test_user):
+def test_user_creation(create_test_user):
     assert create_test_user.id is not None
 
-def test_channel_creation(test_app, create_test_channel):
+def test_channel_creation(create_test_channel):
     assert create_test_channel.id is not None
 
 @pytest.fixture
-def create_test_user_channel(test_app, create_test_user, create_test_channel):
+def create_test_user_channel(create_test_user, create_test_channel):
     test_user = create_test_user
     test_channel = create_test_channel
 
@@ -55,12 +48,13 @@ def create_test_user_channel(test_app, create_test_user, create_test_channel):
     db.session.commit()
     yield user_channel
     db.session.delete(user_channel)
+    db.session.commit()
 
-def test_user_channel_relationship(test_app, create_test_user_channel):
+def test_user_channel_relationship(create_test_user_channel):
     assert create_test_user_channel.id is not None
 
 @pytest.fixture
-def create_test_message(test_app, create_test_user, create_test_channel):
+def create_test_message(create_test_user, create_test_channel):
     test_user = create_test_user
     test_channel = create_test_channel
 
@@ -69,12 +63,13 @@ def create_test_message(test_app, create_test_user, create_test_channel):
     db.session.commit()
     yield message
     db.session.delete(message)
-    
-def test_message_creation(test_app, create_test_message):
+    db.session.commit()
+
+def test_message_creation(create_test_message):
     assert create_test_message.id is not None
 
 @pytest.fixture
-def create_friendship(test_app, create_test_user):
+def create_friendship(create_test_user):
     test_user1 = create_test_user
     test_user2 = create_test_user
 
@@ -83,13 +78,14 @@ def create_friendship(test_app, create_test_user):
     db.session.commit()
     yield friendship
     db.session.delete(friendship)
+    db.session.commit()
 
 
-def test_friendship_creation(test_app, create_friendship):
+def test_friendship_creation(create_friendship):
     assert create_friendship.id is not None
 
 @pytest.fixture
-def create_block(test_app, create_test_user):
+def create_block(create_test_user):
     test_user1 = create_test_user
     test_user2 = create_test_user
 
@@ -98,18 +94,9 @@ def create_block(test_app, create_test_user):
     db.session.commit()
     yield block
     db.session.delete(block)
-
-def test_block_creation(test_app, create_block):
-    assert create_block.id is not None
-
-# Cleaning up test data after tests
-def delete_test_data(test_app):
-    db.session.query(User).filter_by(username="test_user").delete()
-    db.session.query(Channel).filter_by(channel_name="test_channel").delete()
     db.session.commit()
 
-# Register the function to be run after each test
-@pytest.fixture(autouse=True)
-def run_after_test(test_app):
-    yield
-    delete_test_data(test_app)
+def test_block_creation(create_block):
+    assert create_block.id is not None
+
+
