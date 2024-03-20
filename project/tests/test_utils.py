a3cf0d0ea4bd9a2ce50872c43bfb60fc6a240e60
Chris Hill <chrishill_12@hotmail.com>
2024-03-20 13:08:44 -0700

    Fixed test_utils.py

--- project/tests/test_utils.py
+++ project/tests/test_utils.py
@@ -1,20 +1,25 @@
 from flask import session
-from project.utils import get_current_user_id, is_user_channel_admin
 from Database.database_setup import UserChannel
+from project.utils import get_current_user_id, is_user_channel_admin
 
-# Mocking the session for testing purposes
 class MockSession(dict):
     def __init__(self, *args, **kwargs):
         super(MockSession, self).__init__(*args, **kwargs)
+        
+class MockUserChannel:
+    def __init__(self, user_id, channel_id, is_moderator):
+        self.user_id = user_id
+        self.channel_id = channel_id
+        self.is_moderator = is_moderator
 
-def test_get_current_user_id():
+def test_get_current_user_id(mocker):
     # Mock a logged-in user session
     with session_scope({'user_id': 123}):
-        assert get_current_user_id() == 123
+        # Call the function to get user ID
+        user_id = get_current_user_id()
 
-    # Test when user is not logged in
-    with session_scope({}):
-        assert get_current_user_id() is None
+        # Assert the user ID is as expected
+        assert user_id == 123
 
 def test_is_user_channel_admin():
     # Mock UserChannel data - User is an admin
@@ -24,21 +29,13 @@ def test_is_user_channel_admin():
     user_channel_not_admin = MockUserChannel(user_id=2, channel_id=1, is_moderator=False)
     
     # Test case: User is an admin
-    assert is_user_channel_admin(user_id=1, channel_id=1, mock_user_channel=user_channel_admin) is True
+    assert is_user_channel_admin(user_id=1, channel_id=1, user_channel=user_channel_admin) is True
 
     # Test case: User is not an admin
-    assert is_user_channel_admin(user_id=2, channel_id=1, mock_user_channel=user_channel_not_admin) is False
+    assert is_user_channel_admin(user_id=2, channel_id=1, user_channel=user_channel_not_admin) is False
 
-# Context manager for mocking session
 def session_scope(session_data):
     original_session = session._get_current_object()
     session._get_current_object = lambda: MockSession(session_data)
     yield
     session._get_current_object = lambda: original_session
-
-# Mocking the UserChannel model for testing purposes
-class MockUserChannel:
-    def __init__(self, user_id, channel_id, is_moderator):
-        self.user_id = user_id
-        self.channel_id = channel_id
-        self.is_moderator = is_moderator
