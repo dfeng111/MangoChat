986548a52a7c72fd5054f4f9bcbf4b1e42626096
Chris Hill <chrishill_12@hotmail.com>
2024-03-20 13:12:50 -0700

    Fixed argument error for utils.py

--- project/utils.py
+++ project/utils.py
@@ -10,20 +10,18 @@ def get_current_user_id():
     """
     return session.get('user_id')
 
-def is_user_channel_admin(user_id, channel_id):
+def is_user_channel_admin(user_id, channel_id, user_channel):
     """
     Check if the user with the given user_id is an admin of the channel with the given channel_id.
 
     Args:
         user_id: ID of the user
         channel_id: ID of the channel
+        user_channel: UserChannel instance to check
 
     Returns:
         True if the user is an admin of the channel, False otherwise.
     """
-    # Query the UserChannel table to check if the user is an admin of the channel
-    user_channel = UserChannel.query.filter_by(user_id=user_id, channel_id=channel_id).first()
-
     # Check if the user_channel exists and the user is a moderator
     if user_channel and user_channel.is_moderator:
         return True
