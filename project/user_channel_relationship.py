81dbbd91bd05e2162da73af5771a21b6b80b0c15
chrishill93 <113214219+chrishill93@users.noreply.github.com>
2024-03-30 17:17:03 -0700

    Refactor: werkzeug, not workzeug

--- project/user_channel_relationship.py
+++ project/user_channel_relationship.py
@@ -1,4 +1,4 @@
-from workzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from database_setup import db, Message
 
 class User(db.Model):
