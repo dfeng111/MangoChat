5fd4311662ba59eb77d307b32da96f99d29bf21d
Jacob Tizel <jacob.tizel@gmail.com>
2024-04-12 15:11:44 -0700

    fix(docker): Fixed shell script to use python correctly.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 docker-compose up --build -d
 
@@ -6,16 +6,21 @@ cd flask
 python3 -m venv mangoflask
 
 directory="./mangoflask/Scripts"
+directory2="./mangoflask/bin"
 
 
 if [ -d "$directory" ]; then
     echo "Scripts dir exists. Running command 1..."
     # Command to execute if directory exists
     source mangoflask/Scripts/activate
-else
+elif [[ -d "$directory2" ]]; then
+    
     echo "bin dir exists. Running command 2..."
     # Command to execute if directory does not exist
     source mangoflask/bin/activate
+else
+    echo "Neither directory found. Aborting."
+    exit 1
 fi
 
 cd ../project
